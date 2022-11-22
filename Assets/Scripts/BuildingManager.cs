3f63152da4f1dfbe09dab365cf4d5be2f4ebbd8c
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-11-22 19:15:56 +0300

    Добавил управление камерой, сделал появление нового объета по центру камеры

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 public class BuildingManager : MonoBehaviour
 {
     [SerializeField] private GameObject prefabObjForBuild;
+    [SerializeField] private Camera mainCamera;
 
     public void CreateObjectForBuild()
     {
-        Instantiate(prefabObjForBuild, Vector3.zero, Quaternion.identity);
+        Vector3 targetPosition = new Vector3();
+        Vector3 rayDirection = new Vector3(Screen.width/2, Screen.height/2, 0);
+        
+        Ray ray = mainCamera.ScreenPointToRay(rayDirection);
+        
+        RaycastHit hit; 
+        Physics.Raycast(ray, out hit);
+
+        targetPosition = hit.point;
+        targetPosition.y = 0.5f;
+        
+        Instantiate(prefabObjForBuild, targetPosition, Quaternion.identity);
     }
 }
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    
+    private Vector3 touch;
+
+    private float minZoom = 1;
+    private float maxZoom = 8;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touch = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroLastPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOneLastPos = touchOne.position - touchOne.deltaPosition;
+
+            float distTouch = (touchZeroLastPos - touchOneLastPos).magnitude;
+            float currentDistTouch = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentDistTouch - distTouch;
+            
+            Zoom(difference * 0.01f);
+        }
+        
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touch - mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            direction.y = 0;
+            mainCamera.transform.position += direction;
+        } 
+        
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+        //transform.position = ClampCamera(transform.position);
+    }
+
+    void Zoom(float increment)
+    {
+        mainCamera.orthographicSize = Mathf.Clamp(mainCamera.orthographicSize - increment, minZoom, maxZoom);
+    }
+
+    private Vector3 ClampCamera(Vector3 targetPosition)
+    {
+        float camHight = mainCamera.orthographicSize;
+        float camWidht = mainCamera.orthographicSize * mainCamera.aspect;
+
+        float minX = -25 + camWidht;
+        float maxX = 25 - camWidht;
+        float minZ = -25 + camHight;
+        float maxZ = 25 - camHight;
+
+        float newX = Mathf.Clamp(targetPosition.x, minX, maxX);
+        float newZ = Mathf.Clamp(targetPosition.y, minZ, maxZ);
+
+        return new Vector3(newX, targetPosition.y, newZ);
+    }
+}
