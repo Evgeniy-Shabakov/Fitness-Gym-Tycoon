0266a9a77d18fd23acea4454a3a56351e6ce488c
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-11-23 18:59:46 +0300

    Продолжид делать систему строительства объектов: сделал функции установки и удаления, проверку на коллизии перед установкой объекта

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -5,8 +5,12 @@ public class BuildingManager : MonoBehaviour
     [SerializeField] private GameObject prefabObjForBuild;
     [SerializeField] private Camera mainCamera;
 
+    private GameObject objectForBuild;
+
     public void CreateObjectForBuild()
     {
+        if (objectForBuild != null) Destroy(objectForBuild);
+        
         Vector3 targetPosition = new Vector3();
         Vector3 rayDirection = new Vector3(Screen.width/2, Screen.height/2, 0);
         
@@ -18,6 +22,26 @@ public class BuildingManager : MonoBehaviour
         targetPosition = hit.point;
         targetPosition.y = 0.5f;
         
-        Instantiate(prefabObjForBuild, targetPosition, Quaternion.identity);
+        objectForBuild = Instantiate(prefabObjForBuild, targetPosition, Quaternion.identity);
+        
+        objectForBuild.GetComponent<BoxCollider>().isTrigger = true;
+        objectForBuild.transform.SetParent(mainCamera.transform);
+    }
+
+    public void SetObject()
+    {
+        if (objectForBuild == null) return;
+        if (objectForBuild.GetComponent<ObjectForBuild>().PlaceForBuilIsClear() == false) return;
+            
+        objectForBuild.GetComponent<BoxCollider>().isTrigger = false;
+        objectForBuild.transform.SetParent(null);
+        
+        Destroy(objectForBuild.GetComponent<ObjectForBuild>());
+        objectForBuild = null;
+    }
+
+    public void DeleteObject()
+    {
+        Destroy(objectForBuild);
     }
 }
---
+++ Assets/Scripts/ObjectForBuild.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class ObjectForBuild : MonoBehaviour
+{
+    [SerializeField] private Material red;
+
+    private bool placeForBuildIsClear;
+    
+    private Material defaultMaterial;
+    private MeshRenderer mr;
+
+    private void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+
+        placeForBuildIsClear = true;
+        defaultMaterial = mr.material;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Plane") return;
+
+        placeForBuildIsClear = false;
+        mr.material = red;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        placeForBuildIsClear = true;
+        mr.material = defaultMaterial;
+    }
+
+    public bool PlaceForBuilIsClear()
+    {
+        return placeForBuildIsClear;
+    }
+}
