d6bc14e4e13d7123a53a9c2077d3fa5ba0b4a5b6
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-11-30 17:38:12 +0300

    Сделал чтобы объет можно было активировать только при клике как по кнопке ибез смещения камеры

--- Assets/Scripts/ObjectSettings.cs
+++ Assets/Scripts/ObjectSettings.cs
@@ -4,9 +4,24 @@ using UnityEngine.EventSystems;
 
 public class ObjectSettings : MonoBehaviour
 {
-   private void OnMouseUp()
+   private Vector3 cameraPositionMouseDown;
+
+   private void OnMouseDown()
+   {
+      if(EventSystem.current.IsPointerOverGameObject ()) return;
+      
+      if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+      {
+         if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) return;
+      }
+      
+      cameraPositionMouseDown = Camera.main.transform.position;
+   }
+
+   private void OnMouseUpAsButton()
    {
       if (BuildingManager.Instanse.objectForBuild != null) return;
+      if (cameraPositionMouseDown != Camera.main.transform.position) return;
       
       gameObject.AddComponent<PreBuildingCollision>();
       gameObject.AddComponent<PreBuildingMoving>();
