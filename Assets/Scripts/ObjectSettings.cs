734755104f9f17e7777422ca71ab84ecbc679630
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-12-04 16:30:23 +0300

    Изменил способ захвата и перемещения объекта чтобы объеты можно было захватывать за исчезнувшими стенами

--- Assets/Scripts/ObjectSettings.cs
+++ Assets/Scripts/ObjectSettings.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 public class ObjectSettings : MonoBehaviour
 {
    private Vector3 cameraPositionMouseDown;
-
-   private void OnMouseDown()
+   
+   private LayerMask layerMaskObjects;
+   
+   private void Start()
+   {
+      layerMaskObjects =  LayerMask.GetMask("Objects");
+   }
+   
+   /*private void OnMouseDown()
    {
       if (UIManager.Instance.IsPointerOverUIObject()) return;
 
@@ -25,5 +32,36 @@ public class ObjectSettings : MonoBehaviour
       transform.parent.transform.SetParent(Camera.main.transform);
       
       Destroy(gameObject.GetComponent<ObjectSettings>());
+   }*/
+   
+   void Update()
+   {
+      if (UIManager.Instance.IsPointerOverUIObject()) return;
+      if (Input.touchCount >= 2) return;
+        
+      if (Input.GetMouseButtonDown(0)) cameraPositionMouseDown = Camera.main.transform.position;
+
+      if (Input.GetMouseButtonUp(0))
+      {
+         if (Camera.main.transform.position != cameraPositionMouseDown) return;
+            
+         Ray ray = new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward);
+         RaycastHit hit; 
+         
+         if (Physics.Raycast(ray, out hit, 100f, layerMaskObjects) == false) return;
+         if (hit.transform.gameObject != gameObject) return;
+         
+         if (BuildingManager.Instance.objectForBuild != null) return;
+         if (cameraPositionMouseDown != Camera.main.transform.position) return;
+      
+         gameObject.AddComponent<PreBuildingCollision>();
+         gameObject.AddComponent<PreBuildingMoving>();
+
+         BuildingManager.Instance.objectForBuild = transform.parent.gameObject;
+         gameObject.GetComponentInChildren<BoxCollider>().isTrigger = true;
+         transform.parent.transform.SetParent(Camera.main.transform);
+      
+         Destroy(gameObject.GetComponent<ObjectSettings>());
+      }
    }
 }
--- Assets/Scripts/PreBuildingMoving.cs
+++ Assets/Scripts/PreBuildingMoving.cs
@@ -8,14 +8,20 @@ public class PreBuildingMoving : MonoBehaviour
     private Vector3 touch;
     
     private GameObject parent;
+    private LayerMask layerMaskObjects;
+
+    private bool objectIsСaptured;
+    
 
     private void Start()
     {
         parent = transform.parent.gameObject;
+        layerMaskObjects =  LayerMask.GetMask("Objects");
+        objectIsСaptured = false;
     }
     
 
-    private void OnMouseDrag()
+    /*private void OnMouseDrag()
     {
         Camera.main.gameObject.GetComponent<CameraController>().enabled = false;
         
@@ -29,17 +35,32 @@ public class PreBuildingMoving : MonoBehaviour
     private void OnMouseUp()
     {
         Camera.main.gameObject.GetComponent<CameraController>().enabled = true;
-    }
+    }*/
 
     void Update()
     {
         if (UIManager.Instance.IsPointerOverUIObject()) return;
         if (Input.touchCount >= 2) return;
-        
-        if (Input.GetMouseButtonDown(0)) cameraPositionMouseDown = Camera.main.transform.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            cameraPositionMouseDown = Camera.main.transform.position;
+            
+            Ray ray = new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 100f, layerMaskObjects) == false) return;
+            if (hit.transform.gameObject != gameObject) return;
+            
+            Camera.main.gameObject.GetComponent<CameraController>().enabled = false;
+            objectIsСaptured = true;
+        }
 
         if (Input.GetMouseButtonUp(0))
         {
+            Camera.main.gameObject.GetComponent<CameraController>().enabled = true;
+            objectIsСaptured = false;
+            
             if (Camera.main.transform.position != cameraPositionMouseDown) return;
             
             Ray ray = new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward);
@@ -47,6 +68,17 @@ public class PreBuildingMoving : MonoBehaviour
             Physics.Raycast(ray, out hit, 100f, BuildingManager.Instance.layerMaskForPlane);
 
             parent.transform.position = new Vector3(hit.point.x, 0f, hit.point.z);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (objectIsСaptured == false) return;
+            
+            Ray ray2 = new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), Camera.main.transform.forward);
+            RaycastHit hit2; 
+            Physics.Raycast(ray2, out hit2, 100f, BuildingManager.Instance.layerMaskForPlane);
+
+            parent.transform.position = new Vector3(hit2.point.x, 0f, hit2.point.z);
         }
     }
 }
