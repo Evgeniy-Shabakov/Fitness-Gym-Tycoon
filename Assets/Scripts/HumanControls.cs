c76c76bf11a8a3ffb05981214f268611c73f4945
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-12-08 15:02:36 +0300

    Делаю алгоритм движения клиента в спортзале

--- Assets/Scripts/HumanControls.cs
+++ Assets/Scripts/HumanControls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,16 +8,63 @@ public class HumanControls : MonoBehaviour
 {
     private Camera mainCamera;
     private NavMeshAgent navMeshAgent;
+
+    private GameObject parentAllDynamicObjects;
     
-    // Start is called before the first frame update
+    private int countTargets;
+    private int[] targetsIndexes;
+    
     void Start()
     {
         mainCamera = Camera.main;
         navMeshAgent = GetComponent<NavMeshAgent>();
+        
+        parentAllDynamicObjects = GameObject.Find("DynamicObjectsForSaveLoad");
+
+        countTargets = 10;
+        targetsIndexes = new int[countTargets];
+        
+        Invoke("MoveHuman", 2f);
     }
 
+    private void MoveHuman()
+    {
+        targetsIndexes[0] = 0;
+        
+        for (int i = 1; i < countTargets; i++)
+        {
+            targetsIndexes[i] = Random.Range(1, BuildingManager.Instance.objectsForBuilding.Count);
+        }
+
+        StartCoroutine("DelayMovingHuman");
+    }
+
+    IEnumerator DelayMovingHuman()
+    {
+        for (int i = 0; i < countTargets; i++)
+        {
+            Vector3 targetPosition = transform.position;
+        
+            foreach(Transform child in parentAllDynamicObjects.transform)
+            {
+                if (child.GetComponentInChildren<ObjectData>().indexInBuildingManagerList == targetsIndexes[i])
+                {
+                    if (i == 0) targetPosition = child.position - Vector3.forward;
+                    else targetPosition = child.position;
+                    break;
+                }
+            }
+        
+            navMeshAgent.SetDestination(targetPosition);
+            
+            yield return new WaitForSeconds(15f);
+        }
+        
+        navMeshAgent.SetDestination(Vector3.zero);
+    }
+    
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -27,7 +75,6 @@ public class HumanControls : MonoBehaviour
             {
                 navMeshAgent.SetDestination(hit.point);
             }
-            
         }
-    }
+    }*/
 }
