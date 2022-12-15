351bd986a43e7c384335fae38541dbcd8c0891de
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-12-15 19:42:21 +0300

    Отмена

--- Assets/Scripts/HumanClient/PanelHumanClient.cs
+++ Assets/Scripts/HumanClient/PanelHumanClient.cs
@@ -31,8 +31,6 @@ public class PanelHumanClient : MonoBehaviour
 
     public void OnMouseUpAsButton()
     {
-        if (UIManager.Instance.IsPointerOverUIObject()) return;
-        
         CameraController.Instance.objectForFollow = gameObject;
         UIManager.Instance.currentGameObjectForPanelHumanClient = gameObject;
         
