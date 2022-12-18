b67085df9bcf3b3403526012b70cb420078a4588
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2022-12-18 16:08:50 +0300

    Картинки над клиентом: исправлена ошибка с неверным расположением на некоторых тренажерах

--- Assets/Scripts/HumanClient/HumanCanvasRotation.cs
+++ Assets/Scripts/HumanClient/HumanCanvasRotation.cs
@@ -6,16 +6,20 @@ public class HumanCanvasRotation : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
     private Camera mainCamera;
+
+    private Vector3 canvasTransformPosOnStart;
     
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+        canvasTransformPosOnStart = canvas.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         canvas.transform.rotation = mainCamera.transform.rotation;
+        canvas.transform.position = transform.position + canvasTransformPosOnStart;
     }
 }
